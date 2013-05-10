13f9805c37dd9d9b4e6bc23209318690a2f692f1
Amol Kelkar <kelkar.amol@gmail.com>
2013-05-10 13:55:17 -0700

    Don't let production setting for paperclip apply to development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,9 @@ Ragafied::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 
   # Log the query plan for queries taking more than this (works
